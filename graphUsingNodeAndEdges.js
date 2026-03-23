0631dcd1c7b3388348ec4149ca27e997428d5cb3
Shubham Jain <shubhamjain2796@gmail.com>
2026-03-23 10:28:26 +0530

    Refactor graph functions and add Map support
    Refactor graph building functions to support both directed and undirected graphs. Added a new function to build a graph using a Map for non-string keys.

--- graphUsingNodeAndEdges.js
+++ graphUsingNodeAndEdges.js
@@ -1,10 +1,9 @@
-
-// Build undirected graph with nodes and edges given
-nodes = ["A", "B", "C"];
-edges = [
-    ["A", "B"],
-    ["A", "C"]
-];
+// Build directed graph with nodes and edges given
+// nodes = ["A", "B", "C"];
+// edges = [
+//     ["A", "B"],
+//     ["A", "C"]
+// ];
 
 function buildGraph(nodes, edges) {
     this.graph = {};
@@ -19,13 +18,31 @@ function buildGraph(nodes, edges) {
 
 function buildGraphBetter(nodes, edges) {
     this.graph = {};
-    for(let n of nodes){
-        this.graph[n]=[];
+    for (let n of nodes) {
+        this.graph[n] = [];
     }
-    for( let e of edges){
+    for (let e of edges) {
         this.graph[e[0]].push(e[1]);
     }
     return this.graph;
 }
 
-console.log(buildGraphBetter(nodes, edges));
+// map -> used for non string keys.
+const user1 = { id: 1, name: "Shubham" };
+const user2 = { id: 2, name: "Rahul" };
+const user3 = { id: 3, name: "Jain" }
+const nodes =[user1, user2, user3];
+const edges =[[user1, user2], [user1, user3]];
+
+function buildGraphUsingMap(nodes, edges) {
+    this.graph = new Map();
+    for (let node of nodes) {
+        this.graph.set(node, []);
+    }
+    for (let edge of edges) {
+        this.graph.get(edge[0]).push(edge[1]);
+    }
+    return this.graph;
+}
+
+console.log(buildGraphUsingMap(nodes, edges));
