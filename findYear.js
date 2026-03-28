bac584d52de94700fab50a1b8544583104fe362f
shubhamjaintech <shubhamjain2796@gmail.com>
2026-03-28 08:36:58 +0530

    feat: findYear flatten array approach

--- findYear.js
+++ findYear.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 // complexity: O(Y*12)
 function findYear(target) {
-const months = [
-    'January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'
-];
+    const months = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
     const targetNum = Number(target);
     for (let i = 1000; i < 9334; i++) {
         const yearDir = path.join(__dirname, 'data', String(i));
@@ -48,12 +48,17 @@ const ar = [1, 2, 3, 4, 5, 6];
 // console.log(binarySearch(ar, 5));
 
 function getMonthValue(filePath) {
-    const content = fs.readFileSync(filePath, 'utf8');
-    const m = content.match(/\[\[\s*Number of people:\s*(\d+)\s*\]\]/i);
-    if (m) {
-        const fileValue = Number(m[1]);
-        return fileValue;
+    try {
+        const content = fs.readFileSync(filePath, 'utf8');
+        const m = content.match(/\[\[\s*Number of people:\s*(\d+)\s*\]\]/i);
+        if (m) {
+            const fileValue = Number(m[1]);
+            return fileValue;
+        }
+    } catch (err) {
+        console.log(err);
     }
+
     return null;
 }
 const months = [
@@ -64,6 +69,10 @@ const months = [
 function findYearBetter(target) {
     let l = 1000;
     let h = 9333;
+    const months = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
     while (l <= h) {
         let mid = Math.floor((l + h) / 2);
         const yearDir = path.join(__dirname, 'data', String(mid));
@@ -87,4 +96,26 @@ function findYearBetter(target) {
     return -1;
 }
 
-console.log(findYearBetter(34154184));
+// Complexity: O((endYear - startYear) * 12)
+function findYearFlatten(target) {
+    const startYear = 1000;
+    const endYear = 9333;
+    let l = 0;
+    let h = ((endYear - startYear) * 12 ) -1;
+    while (l <= h) {
+        let mid = Math.floor((l + h) / 2);
+        let row = startYear + Math.floor(mid / 12);
+        let col = mid % 12;
+        const filePath = path.join(__dirname, 'data', String(row), `${months[col]}.txt`)
+        const v = getMonthValue(filePath);
+        if(v==null){return -1};
+        if (v === target) {
+            return row;
+        } else if (target < v) {
+            h = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+}
+console.log(findYearFlatten(50109608));
